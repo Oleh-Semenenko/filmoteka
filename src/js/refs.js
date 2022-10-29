e40f4e3da2032e55b30970257627c43ec75eaf12
Your Name <eugene.sidorov@me.com>
2022-10-29 14:30:09 +0300

    add spinner

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,6 @@
 export const refs = {
   moviesList: document.querySelector('main'),
   form: document.querySelector('#search-form'),
+  spinner: document.querySelector('.js-spinner'),
+  load: document.querySelector('[data-load]'),
 };
