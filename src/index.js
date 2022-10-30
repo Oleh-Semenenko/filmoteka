5f9cd7faf6e5593d2e43cbdcef22fa000b96816f
Oleh-Semenenko <o.semenenko.mar@gmail.com>
2022-10-30 11:20:32 +0200

    fix refs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { refs } from './js/refs';
-// import './js/layout/header';
 import Movies from './js/Movies';
 import renderMarkupByKeywordMoviesList from './js/render-markup-by-keyword';
 import genresOfMovies from './data/genresOfMovies.json';
