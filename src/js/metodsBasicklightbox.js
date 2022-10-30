b5f4723f15b655103040f6c9039f9057b47c919b
Oleh-Semenenko <o.semenenko.mar@gmail.com>
2022-10-30 22:00:57 +0200

    first commit

--- src/js/metodsBasicklightbox.js
+++ src/js/metodsBasicklightbox.js
@@ -1,5 +1,6 @@
 const basicLightbox = require('basiclightbox');
 import * as basicLightbox from 'basiclightbox';
+import makeFilmModalMarkup from './modal-film';
 
 const btn = document.querySelector('.template');
 
@@ -17,22 +18,60 @@ export default async function openModalOnClick(data) {
     overview,
   } = await data;
 
+  console.log(makeFilmModalMarkup(data));
+
   const instance = basicLightbox.create(
-    `
-      <div class="movie__item" data-id="${id}">
-  <img src="${poster_path}" class="movie__image" data-id="${id}" alt="Poster movie"  width="" height="" />
-    <div class="movie__description" data-id="${id}">
-      <h2 class="movie__title" data-id="${id}">${title}</h2>
-      <p class="movie__info" data-id="${id}">${vote_count}</p>
-      <p class="movie__info" data-id="${id}">${popularity}</p>
-      <p class="movie__info" data-id="${id}">${original_title}</p>
-      <p class="movie__info" data-id="${id}">${genres
-      .map(elem => elem.name)
-      .join(', ')}</p>
-        <p class="movie__info" data-id="${id}">${overview}</p>
-    </div>
-  </a>
-</div>`,
+    `<div class="modal">
+        <button type="button" class="modal__close-btn">
+            <svg class="" width="14" height="14">
+                <use href="./images/symbol-defs.svg#icon-close-black"></use>
+            </svg>
+        </button>
+    <div class="film__image">${
+      poster_path !== null
+        ? `<img class="image" src="${poster_path}" alt=${title}/>`
+        : ''
+    }
+      </div>
+      <div class="film__information">
+          <h2 class="film__title">${title}</h2>
+          <ul class="film__list">
+            <li class="film__item">
+              <p class="film__details">Vote / Votes</p>
+              <p class="film__info--uper">
+                <span class="film__rating--orange">${vote_average}</span>
+                <span class="film__rating--divider"> / </span>
+      <span class="vote-count">${vote_count}</span>
+              </p>
+            </li>
+            <li class="film__item">
+              <p class="film__details">Popularity</p>
+              <p class="film__info--uper">${popularity}</p>
+            </li>
+            <li class="film__item">
+              <p class="film__details">Original title</p>
+              <p>${title}</p>
+            </li>
+            <li class="film__item">
+              <p class="film__details">Genre</p>
+              <p class="film__info">${genres}</p>
+            </li>
+          </ul>
+        <div class="film__about">
+          <h3 class="film__about-title">About</h3>
+          ${
+            overview
+              ? `<p class="film__about-text">${overview}</p>`
+              : `<p class="film__about-text">No information</p>`
+          }
+        </div>
+        <div class="button-wrapper">
+          <button type="button" class="film__button btn__watch btn__watch__remove">Add to watched</button>
+          <button type="button" class="film__button btn__queue btn__queue__remove">Add to queue</button>
+          <button type="button" class="film__button btn__watch btn__watch__remove">Add to watched</button>
+          </div>
+        </div>
+        </div>`,
     {
       onShow: instance => {
         document.addEventListener('keydown', e =>
