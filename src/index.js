fc009284e166bb764546a6196fc284c3689fa659
Oleh-Semenenko <o.semenenko.mar@gmail.com>
2022-10-30 10:13:03 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import Movies from './js/Movies';
 import renderMarkupByKeywordMoviesList from './js/render-markup-by-keyword';
 import genresOfMovies from './data/genresOfMovies.json';
 import './js/pagination';
+
