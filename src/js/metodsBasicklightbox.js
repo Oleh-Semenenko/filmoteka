e8181446f04f446a467463f2af65b5d0a71017cc
Oleh-Semenenko <o.semenenko.mar@gmail.com>
2022-10-31 09:47:30 +0200

    open modal

--- src/js/metodsBasicklightbox.js
+++ src/js/metodsBasicklightbox.js
@@ -18,57 +18,53 @@ export default async function openModalOnClick(data) {
     overview,
   } = await data;
 
-  console.log(makeFilmModalMarkup(data));
-
   const instance = basicLightbox.create(
-    `<div class="modal">
+    `
+    <div class="modal">
         <button type="button" class="modal__close-btn">
             <svg class="" width="14" height="14">
                 <use href="./images/symbol-defs.svg#icon-close-black"></use>
             </svg>
         </button>
-    <div class="film__image">${
-      poster_path !== null
-        ? `<img class="image" src="${poster_path}" alt=${title}/>`
-        : ''
-    }
+      <div class="film__image">
+      <img class="image" src="${poster_path}" alt=${title} data-id=${id}/>
+      <button type="button" class="film__button btn__trailer">Watch trailer</button>
       </div>
       <div class="film__information">
-          <h2 class="film__title">${title}</h2>
+          <h2 class="film__title" data-id=${id}>${title}</h2>
           <ul class="film__list">
             <li class="film__item">
               <p class="film__details">Vote / Votes</p>
               <p class="film__info--uper">
-                <span class="film__rating--orange">${vote_average}</span>
+                <span class="film__rating--orange" data-id=${id}>${vote_average}</span>
                 <span class="film__rating--divider"> / </span>
-      <span class="vote-count">${vote_count}</span>
+      <span class="vote-count" data-id=${id}>${vote_count}</span>
               </p>
             </li>
             <li class="film__item">
               <p class="film__details">Popularity</p>
-              <p class="film__info--uper">${popularity}</p>
+              <p class="film__info--uper" data-id=${id}>${popularity}</p>
             </li>
             <li class="film__item">
               <p class="film__details">Original title</p>
-              <p>${title}</p>
+              <p data-id=${id}>${title}</p>
             </li>
             <li class="film__item">
               <p class="film__details">Genre</p>
-              <p class="film__info">${genres}</p>
+              <p class="film__info" data-id=${id}>${genres}</p>
             </li>
           </ul>
         <div class="film__about">
           <h3 class="film__about-title">About</h3>
           ${
             overview
-              ? `<p class="film__about-text">${overview}</p>`
+              ? `<p class="film__about-text" data-id=${id}>${overview}</p>`
               : `<p class="film__about-text">No information</p>`
           }
         </div>
         <div class="button-wrapper">
           <button type="button" class="film__button btn__watch btn__watch__remove">Add to watched</button>
           <button type="button" class="film__button btn__queue btn__queue__remove">Add to queue</button>
-          <button type="button" class="film__button btn__watch btn__watch__remove">Add to watched</button>
           </div>
         </div>
         </div>`,
