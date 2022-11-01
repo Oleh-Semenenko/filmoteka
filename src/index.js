3b848d526f5fbda5aa827c908e71ea82c94b8e4c
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-01 18:02:57 +0300

    Закоментував файл modal-film.js
    Цей фейл не потрібен в роботі. Необхідно буде видалити.

--- src/index.js
+++ src/index.js
@@ -3,26 +3,6 @@ import Movies from './js/Movies';
 import renderMarkupByKeywordMoviesList from './js/render-markup-by-keyword';
 import genresOfMovies from './data/genresOfMovies.json';
 import modal from './js/open-modal';
-import makeFilmModalMarkup from './js/modal-film';
+// import makeFilmModalMarkup from './js/modal-film';
 import './js/pagination';
 import './js/button-up';
-
-
-// Для тестирования модалки -> раскомментировать код ниже -> снять класс is-hidden с backdrop в паршеле modal-film.html
-
-// refs.modalEl.insertAdjacentHTML(
-//   'beforeend',
-//   makeFilmModalMarkup(
-//     '',
-//     'Black Adam',
-//     7.077,
-//     602,
-//     3835.264,
-//     [28, 878, 14],
-//     'Nearly 5,000 years after he was bestowed with the almighty powers Nearly 5,000 years after he was bestowed with the almighty powers Nearly 5,000 years after he was bestowed with the almighty powers Nearly 5,000 years after he was bestowed with the almighty powers Nearly 5,000 years after he was bestowed with the almighty powers Nearly 5,000 years after he was bestowed with the ',
-//     ''
-//   )
-// );
-
-
-
