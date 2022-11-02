8b667f055adf4c8a3ef737a92b6357075d30776d
Oleh Semenenko <103896561+Oleh-Semenenko@users.noreply.github.com>
2022-11-02 10:36:15 +0200

    Update open-modal.js

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,20 +1,20 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Movies from './Movies';
+// import GetPrimoryInfoMovie from './API-GET-DETALS-MOVIE';
 import openModalOnClick from './metodsBasicklightbox';
+// import modalTrailerMovie from './modalTrailerMovie';
 import { refs } from './refs.js';
 
 refs.moviesList.addEventListener('click', openModal);
 
-
-function openModal(e) {
+async function openModal(e) {
   e.preventDefault();
   const id = e.target.dataset.id;
 
   const getMovieInfo = new Movies({
-    url: `https://api.themoviedb.org/3/movie/${id}`,
+    url: https://api.themoviedb.org/3/movie/${id},
     params: { api_key: '084c550b6f1767443109bcf4bcaee21b' },
   });
-
 
   try {
     const data = await getMovieInfo.fetchMovies();
@@ -23,5 +23,4 @@ function openModal(e) {
     return Notify.failure(error.message);
   }
 }
-
 
