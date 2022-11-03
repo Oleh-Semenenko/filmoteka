0bbc84323053a053e9ce68500c632d758799bd91
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-11-03 14:27:16 +0200

    небольшие правки в my-library
    добавил рейтинг фильмов в библиотеке и  вывод фильмов при переходе на my-Lybrary

--- src/js/watched-queue-films.js
+++ src/js/watched-queue-films.js
@@ -39,6 +39,8 @@ if (!savedWatched || parcedWatchedFilms.length === 0) {
   }
 }
 
+export default onWatchBtnClick;
+
 if (!savedQueue || parcedWatchedFilms.length === 0) {
   return;
 } else {
@@ -66,25 +68,36 @@ if (!savedQueue || parcedWatchedFilms.length === 0) {
 
 function renderMovie(movies) {
   return movies
-    .map(({ poster_path, original_title, release_date, genres, id }) => {
-      console.log(genres);
-      return `
+    .map(
+      ({
+        poster_path,
+        original_title,
+        release_date,
+        genres,
+        id,
+        vote_average,
+      }) => {
+        console.log(genres);
+        return `
       <li class="movie__item" data-id="${id}">
   <a class="movie__link" data-id="${id}" href="">
   <img src="https://image.tmdb.org/t/p/w342${poster_path}" class="movie__image" data-id="${id}" alt="Poster movie ${original_title}"  width="" height="" />
     <div class="movie__description" data-id="${id}">
       <p class="movie__title" data-id="${id}">${original_title}</p>
       <p class="movie__info" data-id="${id}">${genres
-        .map(elem => elem.name)
-        .join(', ')}  
+          .map(elem => elem.name)
+          .join(', ')}  
       <span class="movie__breacker" data-id="${id}"> | </span>
     <span class="movie__year" data-id="${id}">${release_date
-        .split('-')
-        .slice(0, 1)
-        .join('')}</span></p>
+          .split('-')
+          .slice(0, 1)
+          .join(
+            ''
+          )}</span><span class="movie__rating">${vote_average}</span></p>
     </div>
   </a>
 </li>`;
-    })
+      }
+    )
     .join('');
 }
