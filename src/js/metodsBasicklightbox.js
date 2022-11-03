1dd81b698fdd753415e468592d3b95d7282d15f1
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-03 14:58:11 +0300

    Update metodsBasicklightbox.js
    Підключення імпорту стоп скрола, видалення стоп-скрола з винесенням в окремий файл, видалення зайвого іпорту

--- src/js/metodsBasicklightbox.js
+++ src/js/metodsBasicklightbox.js
@@ -2,9 +2,10 @@ const basicLightbox = require('basiclightbox');
 import * as basicLightbox from 'basiclightbox';
 import getDataTrailerMovie from './getDataTrailerMovie';
 import onModalTrailerMovie from './onModalTrailerMovie';
-import { refs } from './refs';
+
 import initStorageBtns from './initStorageBtns';
 import svg from '../images/symbol-defs.svg';
+import { showModal, closeModal } from './stopScrolWhenModalOpen';
 
 export default async function openModalOnClick(data) {
   const {
@@ -132,28 +133,3 @@ function closeKeyDownKeyEsc(e, instance) {
     instance.close();
   }
 }
-
-function showModal() {
-  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-
-  refs.body.style.position = 'fixed';
-  refs.body.style.top = `-${scrollY}`;
-  refs.body.style.right = `0`;
-  refs.body.style.left = `0`;
-  refs.body.style.paddingRight = `20px`;
-}
-
-function closeModal() {
-  const scrollY = refs.body.style.top;
-  refs.body.style.position = '';
-  refs.body.style.top = '';
-  refs.body.style.paddingRight = `0`;
-  window.scrollTo(0, parseInt(scrollY || '0') * -1);
-}
-
-window.addEventListener('scroll', () => {
-  document.documentElement.style.setProperty(
-    '--scroll-y',
-    `${window.scrollY}px`
-  );
-});
